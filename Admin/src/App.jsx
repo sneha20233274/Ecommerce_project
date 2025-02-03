e76654defdd8a0c3ae04237c9df094ccbc00a0dd
Sneha <itschaudhary@gmail.com>
2025-02-03 14:47:36 +0530

    Resolved errors in Admin panel

---
+++ Admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../src/Components/Navbar/Navbar'
+import Admin from '../src/Pages/Admin/Admin'
+const App = () => {
+  return (
+      <div>
+       <Navbar/>
+       <Admin/>
+      </div>
+  ) 
+}
+export default App
